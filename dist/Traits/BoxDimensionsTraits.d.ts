5ef734d6acef6d80c674f41622f0f1b0a25fc2b5
Nanda <nanda.sankaran@gmail.com>
2022-03-28 13:08:39 +1100

    Remove unused type file.

--- dist/Traits/BoxDimensionsTraits.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModelTraits } from "terriajs-plugin-api";
-export default class BoxDimensionsTraits extends ModelTraits {
-    length?: number;
-    width?: number;
-    height?: number;
-}
